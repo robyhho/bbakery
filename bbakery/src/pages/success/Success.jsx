fe94a8f1ecb768090ba3ade26035c4e446a2f9c3
Robbie Ho <69515266+robyhho@users.noreply.github.com>
2022-10-21 12:41:52 +0100

    updated success.jsx & css

--- bbakery/src/pages/success/Success.jsx
+++ bbakery/src/pages/success/Success.jsx
@@ -7,12 +7,14 @@ const Success = () => {
         <h1 className={styles.logo}>Dantat</h1>
       </header>
       <main className={styles.main}>
-        <h2>Your order has been confirmed and will be shipping soon!</h2>
+        <h2 className={styles.text}>
+          Your order has been confirmed and will be shipping soon!
+        </h2>
         <section className={styles.orderMeta}>
-          <article className={styles.orderDeets}>
+          <div className={styles.orderDeets}>
             <p className={styles.detailTitle}>Order Date</p>
             <p className={styles.detailvalue}>28 November 2022</p>
-          </article>
+          </div>
           <div className={styles.orderDeets}>
             <p className={styles.detailTitle}>Order Number</p>
             <p className={styles.detailvalue}>UK714922</p>
@@ -27,6 +29,12 @@ const Success = () => {
           </div>
         </section>
         <section className={styles.orderItems}>
+          <div className={styles.item}>
+            <img src="" alt="" />
+            <h2 className={styles.itemName}></h2>
+            <h2 className={styles.itemQty}></h2>
+            <h2 className={styles.itemTotal}></h2>
+          </div>
           <div className={styles.item}>
             <img src="" alt="" />
             <h2 className={styles.itemName}></h2>
