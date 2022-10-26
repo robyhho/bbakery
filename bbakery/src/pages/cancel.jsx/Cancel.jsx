d727865e45e239af4c85e64a08d9a2813558486f
Robbie Ho <69515266+robyhho@users.noreply.github.com>
2022-10-26 12:12:18 +0100

    Added svgs cancelBg & triangles, Linked css in cancel.jsx

--- bbakery/src/pages/cancel.jsx/Cancel.jsx
+++ bbakery/src/pages/cancel.jsx/Cancel.jsx
@@ -1,16 +1,17 @@
 import Navbar from "../../components/navbar/Navbar";
-
+import OrangeBtn from "../../components/orangeBtn/OrangeBtn";
+import styles from "./cancel.module.css";
 const Cancel = () => {
   return (
     <>
       <header>
         <Navbar></Navbar>
-        <main>
-          <h1>
+        <main className={styles.container}>
+          <h1 className={styles.title}>
             Forgot to add something to your cart? <br />
             Swing by our shop.
           </h1>
-          <button>Shop Now</button>
+          <OrangeBtn destination={"/shop"}></OrangeBtn>
         </main>
       </header>
     </>
